9c98512c06f04bd73149277765623a80955231c6
Alan Wang <1967878682w@gmail.com>
2026-02-20 14:14:35 -0500

    added shadcn

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [message, setMessage] = useState('')
@@ -10,7 +11,12 @@ function App() {
       .then(data => setMessage(data.message))
   }, [])
 
-  return <p className='font-bold p-4'>{message}</p>
+  return (
+    <>
+      <p className='font-bold p-4'>{message}</p>
+      <Button>Click me</Button>
+    </>
+)
 }
 
 export default App
