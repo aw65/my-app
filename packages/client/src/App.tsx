245512789f78498b2e8099626cd8b31377c194ef
Alan Wang <1967878682w@gmail.com>
2026-02-24 16:56:11 -0500

    Setup Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
    return (
       <>
          <p className="font-bold p-4">{message}</p>
-         <Button>Click me</Button>
+         <Button>Click me!</Button>
       </>
    );
 }
