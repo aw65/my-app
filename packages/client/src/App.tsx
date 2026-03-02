3a80a6ed5ce4c1e11196346d171c8bf44c7741e1
Alan Wang <1967878682w@gmail.com>
2026-03-01 19:01:36 -0500

    front end

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,10 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Button } from './components/ui/button';
+import ChatBot from './components/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
    return (
-      <>
-         <p className="font-bold p-4">{message}</p>
-         <Button>Click me!</Button>
-      </>
+      <div className="p-4 h-screen w-full">
+         <ChatBot />
+      </div>
    );
 }
 
